600658d5a54a1bd28963cc0a422a58b2ff884b09
Eduard S. <eduardsanou@posteo.net>
2023-10-05 18:07:39 +0200

    WIP: Decoder implementation

--- src/emulator.rs
+++ src/emulator.rs
@@ -119,8 +119,10 @@ impl Emulator<u64> {
     // TODO
 }
 
+#[derive(Debug, Default)]
 pub enum Opcode {
     // RISC-U supported opcodes
+    #[default]
     Lui,
     Addi,
     Ld,
@@ -137,6 +139,7 @@ pub enum Opcode {
     Ecall,
 }
 
+#[derive(Debug, Default)]
 pub struct Instruction {
     op: Opcode,
     rd: usize,
@@ -145,24 +148,199 @@ pub struct Instruction {
     imm: i64,
 }
 
-#[repr(u32)]
-enum RvOpcode {
-    LUI = 0b0110111,
-    AUIPC = 0b0010111,
-    JAL = 0b1101111,
-    JALR = 0b1100111,
-    // Conditional Branches
-    BRANCH = 0b1100011, // BEQ, BNE, BLT, BGE, BLTU, BGEU
-    LOAD = 0b0000011,   // LB, LH, LW, LBU, LHU
-    STORE = 0b0100011,  // SB, SH, SW
-    // Integer Register-Immediate Instructions
-    OPIMM = 0b0010011, // ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI
-    // Integer Register Register Operations
-    OP = 0b0110011, // ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
-    MISC_MEM = 0b0001111,
-    SYSTEM = 0b1110011, // ECALL, EBREAK, CSR*
+const MASK_OPCODE: u32 = 0b00000000000000000000000001111111;
+const MASK_FUNCT3: u32 = 0b00000000000000000111000000000000;
+const FUNCT3_JALR: u32 = 0b000;
+const FUNCT3_BRANCH_BEQ: u32 = 0b000;
+const FUNCT3_LOAD_LD: u32 = 0b011;
+const SHIFT_FUNCT3: u32 = 12;
+
+// Opcode encodings
+const RV_OP_LUI: u32 = 0b0110111;
+const RV_OP_AUIPC: u32 = 0b0010111;
+const RV_OP_JAL: u32 = 0b1101111;
+const RV_OP_JALR: u32 = 0b1100111;
+// Conditional Branches
+const RV_OP_BRANCH: u32 = 0b1100011; // BEQ, BNE, BLT, BGE, BLTU, BGEU
+const RV_OP_LOAD: u32 = 0b0000011; // LB, LH, LW, LBU, LHU
+const RV_OP_STORE: u32 = 0b0100011; // SB, SH, SW
+                                    // Integer Register-Immediate Instructions
+const RV_OP_OPIMM: u32 = 0b0010011; // ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI
+                                    // Integer Register Register Operations
+const RV_OP_OP: u32 = 0b0110011; // ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
+const RV_OP_MISC_MEM: u32 = 0b0001111;
+const RV_OP_SYSTEM: u32 = 0b1110011; // ECALL, EBREAK, CSR*
+
+// 'rd' is register destination
+fn decode_rd(ins: u32) -> usize {
+    ((ins & 0b00000000000000000000111110000000) >> 7) as usize
 }
 
-pub fn decode(v: u32) -> Instruction {
-    unimplemented!();
+// 'rs1' is register source 1
+fn decode_rs1(ins: u32) -> usize {
+    ((ins & 0b00000000000011111000000000000000) >> 15) as usize
+}
+
+// 'rs2' is register source 2
+fn decode_rs2(ins: u32) -> usize {
+    ((ins & ins & 0b00000001111100000000000000000000) >> 20) as usize
+}
+
+// 12 bits, sign-extended
+fn decode_i_imm(ins: u32) -> i64 {
+    (((ins & 0b11111111111100000000000000000000) >> 20) as i32) as i64
+}
+
+// 12 bits, sign-extended
+fn decode_s_imm(ins: u32) -> i64 {
+    ((((ins & 0b11111110000000000000000000000000) >> (25 - 5)) as i32)
+        | ((ins & 0b00000000000000000000111110000000) >> 7) as i32) as i64
+}
+// 32 bits, sign-extended
+fn decode_u_imm(ins: u32) -> i64 {
+    ((ins & 0b11111111111111111111000000000000) as i32) as i64
+}
+// 12 bits, sign-extended
+fn decode_b_imm(ins: u32) -> i64 {
+    ((((ins & 0b10000000000000000000000000000000) >> (31 - 12)) as i32)
+        | (((ins & 0b01111110000000000000000000000000) >> (25 - 5)) as i32)
+        | (((ins & 0b00000000000000000000111100000000) >> (8 - 1)) as i32)
+        | (((ins & 0b00000000000000000000000010000000) << -(7 - 11)) as i32)) as i64
+}
+// 32 bits, sign-extended
+fn decode_j_imm(ins: u32) -> i64 {
+    ((((ins & 0b10000000000000000000000000000000) >> (31 - 20)) as i32)
+        | (((ins & 0b01111111111000000000000000000000) >> (21 - 1)) as i32)
+        | (((ins & 0b00000000000100000000000000000000) >> (20 - 11)) as i32)
+        | (((ins & 0b00000000000011111111000000000000) >> (12 - 12)) as i32)) as i64
+}
+
+pub fn decode(ins: u32) -> Instruction {
+    use Opcode::*;
+    match ins & MASK_OPCODE {
+        RV_OP_LUI => {
+            // U-Type
+            let rd = decode_rd(ins);
+            let imm = decode_u_imm(ins);
+            return Instruction {
+                op: Lui,
+                rd,
+                imm,
+                ..Default::default()
+            };
+        }
+        RV_OP_AUIPC => {
+            // U-Type
+            panic!("Unsupported Auipc instruction {:08x}", ins);
+        }
+        RV_OP_JAL => {
+            // J-Type
+            let rd = decode_rd(ins);
+            let imm = decode_j_imm(ins);
+            return Instruction {
+                op: Jal,
+                rd,
+                imm,
+                ..Default::default()
+            };
+        }
+        RV_OP_JALR => {
+            // I-Type
+            let rd = decode_rd(ins);
+            let rs1 = decode_rs1(ins);
+            let imm = decode_i_imm(ins);
+            // jalr
+            match (ins & MASK_FUNCT3) >> SHIFT_FUNCT3 {
+                FUNCT3_JALR => {
+                    return Instruction {
+                        op: Jalr,
+                        rd,
+                        rs1,
+                        imm,
+                        ..Default::default()
+                    };
+                }
+                _ => panic!("Invalid Jalr instruction {:08x}", ins),
+            }
+        }
+        RV_OP_BRANCH => {
+            // B-Type
+            let rs1 = decode_rs1(ins);
+            let rs2 = decode_rs2(ins);
+            let imm = decode_b_imm(ins);
+            match (ins & MASK_FUNCT3) >> SHIFT_FUNCT3 {
+                FUNCT3_BRANCH_BEQ => {
+                    return Instruction {
+                        op: Beq,
+                        rs1,
+                        rs2,
+                        imm,
+                        ..Default::default()
+                    }
+                }
+                _ => panic!("Unsupported/Invalid Jalr instruction {:08x}", ins),
+            }
+        }
+        RV_OP_LOAD => {
+            // I-Type
+            let rd = decode_rd(ins);
+            let rs1 = decode_rs1(ins);
+            let imm = decode_i_imm(ins);
+            match (ins & MASK_FUNCT3) >> SHIFT_FUNCT3 {
+                FUNCT3_LOAD_LD => {
+                    return Instruction {
+                        op: Ld,
+                        rd,
+                        rs1,
+                        imm,
+                        ..Default::default()
+                    }
+                }
+                _ => panic!("Unsupported/Invalid Load instruction {:08x}", ins),
+            }
+        }
+        RV_OP_STORE => {
+            // S-Type
+            let rs1 = decode_rs1(ins);
+            let rs2 = decode_rs2(ins);
+            let imm = decode_s_imm(ins);
+            // sd
+            panic!("Unsupported Store instruction {:08x}", ins);
+        }
+        RV_OP_OPIMM => {
+            // I-Type
+            let rd = decode_rd(ins);
+            let rs1 = decode_rs1(ins);
+            let imm = decode_i_imm(ins);
+            // addi
+            panic!("Unsupported Opimm instruction {:08x}", ins);
+        }
+        RV_OP_OP => {
+            // R-Type
+            let rd = decode_rd(ins);
+            let rs1 = decode_rs1(ins);
+            let rs2 = decode_rs2(ins);
+            // add
+            // sub
+            // mul
+            // divu
+            // remu
+            // sltu
+            panic!("Unsupported Op instruction {:08x}", ins);
+        }
+        RV_OP_MISC_MEM => {
+            panic!("Unsupported MiscMem instruction {:08x}", ins);
+        }
+        RV_OP_SYSTEM => {
+            // I-Type
+            let rd = decode_rd(ins);
+            let rs1 = decode_rs1(ins);
+            let imm = decode_i_imm(ins);
+            // ecall
+            panic!("Unsupported System instruction {:08x}", ins);
+        }
+        _ => {
+            panic!("Invalid instruction {:08x}", ins);
+        }
+    }
 }
